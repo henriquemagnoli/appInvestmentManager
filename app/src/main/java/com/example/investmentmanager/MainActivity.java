2dec6f952605240f2e11b0da4b9f17ee3598f665
henriquemagnoli <87089543+henriquemagnoli@users.noreply.github.com>
2023-09-27 17:06:23 -0300

    Navbar changing pages

--- app/src/main/java/com/example/investmentmanager/MainActivity.java
+++ app/src/main/java/com/example/investmentmanager/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.investmentmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.example.investmentmanager.fragments.HistoryFragment;
+import com.example.investmentmanager.fragments.HomeFragment;
+import com.example.investmentmanager.fragments.WalletFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,8 +34,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void screenLogin(View view) {setContentView(R.layout.login_screen);}
     public void screenSignUp(View view) {setContentView(R.layout.signup_screen);}
-    public void screenHome(View view) {setContentView(R.layout.home_screen);}
-    public void screenWallet(View view) {setContentView(R.layout.wallet_screen);}
-    public void screenHistory(View view) {setContentView(R.layout.history_screen);}
+
+    public void screenNavBar(View view)
+    {
+        setContentView(R.layout.main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.navBar);
+        setFragment(new HomeFragment());
+
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if(id == R.id.btnHome)
+                {
+                    setFragment(new HomeFragment());
+                    return true;
+                }
+                else if (id == R.id.btnWallet)
+                {
+                    setFragment(new WalletFragment());
+                    return true;
+                }
+                else if (id == R.id.btnHistorico)
+                {
+                    setFragment(new HistoryFragment());
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    void setFragment(Fragment fragment)
+    {
+        FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
+        fragTrans.replace(R.id.main_frame, fragment);
+        fragTrans.addToBackStack(null);
+        fragTrans.commit();
+    }
+
     public void screenAddStock(View view) {setContentView(R.layout.addstock_screen);}
 }
